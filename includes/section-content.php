d32cc72125399c13004a00806b03221f6fba3624
kursad333 <45723522+kursad333@users.noreply.github.com>
2020-08-15 16:54:22 +0200

    made page.php

---
+++ includes/section-content.php
@@ -0,0 +1,15 @@
+<div class="page-container">
+    <div class="page-content-wrap">
+        <div class="container">
+            <div class="col-md-12 content">
+                <div class="contentheader">
+                    <div class="main-underline-short">
+                        <h1><?php the_title();?></h1>
+                    </div>
+                </div>
+                <?php if (have_posts() ): while (have_posts() ) : the_post();?>
+                <?php the_content();?>
+                <?php endwhile; else: endif;?>
+            </div>
+        </div>
+    </div>
---
+++ page.php
@@ -0,0 +1,7 @@
+<?php get_header();?>
+
+<?php get_template_part('includes/section', 'content');?>
+
+
+
+<?php get_footer();?>
