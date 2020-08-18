f32789544f8c3b4ea108ee2a4261cd598ba786cd
kursad333 <45723522+kursad333@users.noreply.github.com>
2020-08-19 00:27:25 +0200

    added blog support v1

---
+++ archive.php
@@ -0,0 +1,10 @@
+<?php get_header();?>
+
+<?php get_template_part('includes/sections/section', 'content');?>
+
+
+
+<?php get_footer();?>
+
+
+<!-- //http://localhost:8888/wordpress/2020/08/18/dit-is-mijn-blog/ -->
---
+++ includes/template-blogpost.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Informatieve pagina
+*/
+?>
+
+<?php get_header();?>
+
+
+<?php get_template_part('/includes/sections/section', 'blogpost');?>
+
+
+<?php get_footer();?>
