5ee0edf282a52c4a12fa397f7e4442769550a9c8
kursad333 <45723522+kursad333@users.noreply.github.com>
2020-08-30 01:42:29 +0200

    styling the header

--- includes/sections/section-archive.php
+++ includes/sections/section-archive.php
@@ -1,16 +1,22 @@
 <?php // START PHP
             if (have_posts() ): while (have_posts() ) : the_post();?>
             <div class="col-md-6">
-                <div class="card mb-4">
-                    <div class="card-body">
-                        <div class="text-center">
-                        <h1><?php the_title();?></h1>
-                            </div>
+                <div class="card mb-4" id='blog-post'>
+                    <div class="card-body" id='blog-title'>
+                    <h1><?php the_title();?></h1>
+                    </div>
+
+                    <div class='card-body' id="blog-image" style="background-image: url(https://www.me-to-we.nl/wp-content/uploads/2016/02/Basisschool.jpg)">
+                    </div>
+
+                    <div class="card-body" id="blog-content">
                         <?php the_excerpt();?>
 
-                        <a href="<?php the_permalink();?>" class="btn btn-success">Lees meer</a>
+                        <a href="<?php the_permalink();?>" class="btn btn-primary">Lees meer</a>
                     </div>
                 </div>
             </div>
+
+
 
             <?php endwhile; else: endif;?>
