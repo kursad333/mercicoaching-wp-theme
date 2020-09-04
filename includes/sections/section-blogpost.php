2a736cd6392598a19a7107ed9a79b1e6488668ef
kursad333 <45723522+kursad333@users.noreply.github.com>
2020-09-04 10:43:30 +0200

    styling for blog page finished

--- includes/sections/section-blogpost.php
+++ includes/sections/section-blogpost.php
@@ -5,8 +5,35 @@
                     <div class="main-underline-short">
                         <h1><?php the_title();?></h1>
                     </div>
+                    <div class='authorblock'>
+                        <p> 
+                            Door
+
+                            <?php
+                            //Prints out the name of the author
+                            $name = the_author_meta('display_name', $post->post_author);
+                            echo $name;
+                            ?>
+
+                            op
+
+                            <?php
+                            //Prints out the date the post was published
+                            $date = get_the_date();
+                            echo $date
+                            ?>
+
+                            om
+
+                            <?php
+                            //Prints out the exact time the post was published
+                            $time = the_time();
+                            echo $time;
+                            ?>
+                        </p>
+                    </div>
                 </div>
-                
+
                 <?php // START PHP
                 if (have_posts() ): while (have_posts() ) : the_post();
                     the_content();
