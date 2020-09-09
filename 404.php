17bc88b702443596949cf9fc4124feaaffb15a7c
kursad333 <45723522+kursad333@users.noreply.github.com>
2020-09-09 20:16:21 +0200

    added 404 page

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div class="page-error">
+    <div class="container">
+        <div class="text-center">
+            <h1>404</h1>
+            <hr>
+            <p>Oeps... Deze pagina is niet gevonden.</p>
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer();?>   
